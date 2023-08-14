c60c1b14b4fd19e69a9538f5bfed5c9b07e10ce0
Aaron Lee <alee@aaronosaur.us>
2023-08-13 21:15:04 -0500

    add sending subcommand

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,6 @@
 use std::path::PathBuf;
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -13,10 +13,23 @@ pub struct Args {
     /// Number of times to greet
     #[arg(short, long, default_value_t = true)]
     stream: bool,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
 }
 
 impl Args {
     pub fn load_from_cli() -> Args {
         Args::parse()
+    }
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// sends a single command on the socket
+    Send {
+        /// command to send
+        #[arg(short, long)]
+        payload: String,
     }
 }
